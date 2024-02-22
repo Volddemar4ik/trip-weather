81947e7fddd9828b02ed5febbfbb517b310af0f8
Volddemar4ik <volddemar4ik@gmail.com>
2024-02-22 22:04:12 +0100

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import ModalWindow from './components/modal-window/modal-window';
+import Search from './components/search/search';
+import TripDailyWeather from './components/trip-daily-weather/trip-daily-weather';
+import Sidebar from './components/sidebar/sidebar'
+import TripList from './components/trip-list/trip-list';
+import Header from './components/header/header'
+import { getTripWeather } from './requests';
+import { filterBySearch } from './functions';
+import { tripData } from './defaults';
 import './App.css';
 
 function App() {
+  const [handleSearchText, setHandleSearchText] = useState('')
+  const [modalWindowStatus, setModalWindowStatus] = useState(false)
+  const [currentTripsData, setCurrentTripsData] = useState([])
+  const [currentTrip, setCurrentTrip] = useState({})
+  const [tripDailyWeatherList, setTripDailyWeatherList] = useState([])
+  const [sidebarData, setSidebarData] = useState({ tripStartDate: 0, currentWeatherInCity: {}, })
+
+  useEffect(() => {
+    if (tripData?.length !== 0) {
+      setCurrentTripsData([...tripData])
+    }
+  }, [tripData])
+
+  function handleChangeSearchText(e) {
+    setHandleSearchText(e.target.value)
+  }
+
+  function handleChangeModalWindowStatus() {
+    setModalWindowStatus(prevStatus => !prevStatus)
+  }
+
+  async function handleChangeEveryDayTripWeatcherBlockStatus(currentTripData) {
+    const { city, startDate, endDate } = currentTripData
+
+    const [dailyWeather, todayWeather] = await Promise.all([
+      getTripWeather(city, startDate, endDate),
+      getTripWeather(city)
+    ])
+
+    if (dailyWeather && todayWeather) {
+      setCurrentTrip(currentTripData)
+
+      setTripDailyWeatherList(dailyWeather?.days)
+
+      setSidebarData(prevData => ({
+        ...prevData,
+        tripStartDate: startDate,
+        currentWeatherInCity: todayWeather,
+      }))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+
+      <div className="wrap">
+        <div className="main">
+          <Search
+            handleSearchText={handleSearchText}
+            handleChangeSearchText={handleChangeSearchText}
+          />
+
+          <TripList
+            tripList={filterBySearch(currentTripsData, handleSearchText)}
+            currentTripId={currentTrip?.id}
+            handleChangeEveryDayTripWeatcherBlockStatus={handleChangeEveryDayTripWeatcherBlockStatus}
+            handleChangeModalWindowStatus={handleChangeModalWindowStatus}
+          />
+
+          <div className="main__trip-weather">
+            <div className="main__trip-weather-header">
+              <h2>Weather for each day of the trip to {currentTrip?.city || '(Choose trip)'}</h2>
+            </div>
+
+            <TripDailyWeather tripDailyWeatherList={tripDailyWeatherList} />
+          </div>
+        </div>
+
+        <Sidebar sidebarData={sidebarData} />
+      </div>
+
+      {modalWindowStatus &&
+        <ModalWindow
+          handleChangeModalWindowStatus={handleChangeModalWindowStatus}
+          setCurrentTripsData={setCurrentTripsData}
+        />}
     </div>
   );
 }
 
-export default App;
+export default App
